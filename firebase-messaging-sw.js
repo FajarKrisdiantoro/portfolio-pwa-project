8b9380cdc376a57000c8eb88824a05cac1ab2fb7
FajarKrisdiantoro <fajarkrisdiantoro021205@gmail.com>
2024-11-12 19:36:59 +0700

    maybe its last commit, im tired for push notif , maybe next time

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/10.1.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/11.0.1/firebase-messaging.js');
 
 // Inisialisasi Firebase di Service Worker
 const firebaseConfig = {
